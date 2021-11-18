0b878d2079db60e8bbdddfb33d94989d915562ad
Drdev <fallen.bmxer@gmail.com>
2021-11-18 15:23:54 +0500

    Initial Commit

---
+++ Homework16/Homework16App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework16App.swift
+//  Homework16
+//
+//  Created by Адель Ахметшин on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Homework16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
