9a49ae00a31692b1e3640ceb77a9dd58a7741d50
Drdev <fallen.bmxer@gmail.com>
2021-11-18 15:45:04 +0500

    Setting hierarchy.

---
+++ Homework16/Media.swift
@@ -0,0 +1,20 @@
+//
+//  Media.swift
+//  Homework16
+//
+//  Created by Адель Ахметшин on 18.11.2021.
+//
+
+import SwiftUI
+
+struct Media: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Media_Previews: PreviewProvider {
+    static var previews: some View {
+        Media()
+    }
+}
---
+++ Homework16/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Player.swift
+//  Homework16
+//
+//  Created by Адель Ахметшин on 18.11.2021.
+//
+
+import SwiftUI
+
+struct Player: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Player_Previews: PreviewProvider {
+    static var previews: some View {
+        Player()
+    }
+}
